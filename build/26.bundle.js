49035725c12b40c8fb2c5aeb785fddda8a05603d
odinolson17 <odinolsonsbooks@gmail.com>
2024-01-16 22:54:51 -0500

    Removing unneeded files

--- build/26.bundle.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunksnippets=self.webpackChunksnippets||[]).push([[26],{7026:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
